9df85e68d19579a76849f74e9da4cebb76274fac
nathwoo <nathwoo829@gmail.com>
2024-01-18 11:06:30 +0100

    prometheus

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ import numpy as np
 from flask import Flask, jsonify
 from flask_cors import CORS
 from sklearn.datasets import load_iris
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 iris = load_iris()
 
 app = Flask(__name__)
 CORS(app)
 
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
+
 # Load the pre-trained model
 model = joblib.load('trained_model.joblib')
 
 @app.route('/predict/<sl>/<sw>/<pl>/<pw>', methods=['POST'])
+@metrics.do_not_track()
+@metrics.counter('counter', 'count_of_preds')
 def predict(sl, sw, pl, pw):
     try:
         # Get the input data from the request
